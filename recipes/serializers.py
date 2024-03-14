1bb7d3f759ffae35891ec6385ca98668906a23df
Ruben Sa <rubenjms1@gmail.com>
2024-03-14 09:00:14 -0300

    adicionando meta class para campos dos models

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,26 +1,31 @@
 from rest_framework import serializers
 from tag.models import Tag
+from .models import Recipe
 
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=255)
-    slug = serializers.SlugField()
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
 
-class RecipesSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField(method_name="any_method_name")
-    category = serializers.StringRelatedField()
+class RecipesSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Recipe
+        fields = ['id','title','description','author','category','tags','public','preparation','tag_links']
+    
+    
+    
+    public = serializers.BooleanField(source='is_published', read_only = True)
+    preparation = serializers.SerializerMethodField(method_name="any_method_name", read_only = True)
+    category = serializers.StringRelatedField(read_only = True)
     author = serializers.StringRelatedField()
     tags = serializers.StringRelatedField(many = True)
     
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',
-        queryset=Tag.objects.all(),
+        read_only=True,
         view_name='recipes:recipes_api_v2_tag'
     )
     
