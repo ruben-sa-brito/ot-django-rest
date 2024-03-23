8f76b675870eb12f0b3a7750469a65061a3102a4
Ruben Sa <rubenjms1@gmail.com>
2024-03-22 22:22:20 -0300

    corrigindo bug no allowed_hosts

--- project/settings/environment.py
+++ project/settings/environment.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-
 from utils.environment import get_env_variable, parse_comma_sep_str_to_list
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -10,9 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
     get_env_variable('CSRF_TRUSTED_ORIGINS')
